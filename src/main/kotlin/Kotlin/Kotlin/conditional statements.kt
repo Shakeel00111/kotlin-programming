0ecd0bbb76e89094f186718cbdcda7518efb5c55
[ms] <[namohammedshakeel21@gmail.com]>
2023-06-04 20:30:20 +0530

    new update

--- src/main/kotlin/Kotlin/Kotlin/conditional statements.kt
+++ src/main/kotlin/Kotlin/Kotlin/conditional statements.kt
@@ -1,2 +1,57 @@
-package Kotlin.Kotlin
 
+fun main() {
+    var num1 = 100
+    var num2 = 200
+    var num3 = 300
+
+    if (num1 >= 0) {
+
+        println("$num1 is +")
+
+    } else{
+
+        println("$num1 is -")
+
+    }
+
+
+    if(num1 > num2 && num1> num3){
+        println("$num1 is the greater")
+    } else if(num2 > num1 && num2 > num3){
+        println("$num2 is the greater")
+    }else{
+
+        println("$num3 is the greater")
+    }
+
+    //Nested if else
+    if(num1>num2){
+
+        if(num1>num3){
+            println("$num1 is the greatest")
+        }else{
+
+            println("$num3 is the greatest")
+
+        }
+
+    } else if(num2>num1){
+
+        if(num2>num3){
+
+            println("$num2 is the greatest")
+
+        }else{
+
+            println("$num3 is the greatest")
+
+        }
+
+    }
+    else{
+
+        println("$num3 is the greatest")
+
+    }
+
+}
--- src/main/kotlin/Kotlin/Kotlin/extension.kt
+++ src/main/kotlin/Kotlin/Kotlin/extension.kt
@@ -1,2 +1,23 @@
-package Kotlin.Kotlin
+fun Int.add(num:Int):Int{
+    return this + num
+}
+fun Double.add(num1:Double):Double {
+    return this + num1
+}
+fun Int.sub(num3:Int):Int{
+    return this - num3
+}
 
+fun main(){
+    var sum = 10.add(20)
+    var sum1 = 23.4.add(28.2)
+    var sum3 = 80.sub(20)
+
+    println(sum)
+    println(sum1)
+    println(sum3)
+}
+
+// all extension functions are infix function
+// all infix function are extinction function
+// infix function is an single parameter function
